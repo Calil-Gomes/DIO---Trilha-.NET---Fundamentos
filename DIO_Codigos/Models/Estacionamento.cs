28f94c2e43e9874a14352a7e73e78297e917e3b8
Calil Gomes <75040393+Calil-Gomes@users.noreply.github.com>
2025-06-01 15:03:38 -0300

    Add files via upload

---
+++ DIO_Codigos/Models/Estacionamento.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace DIO_Codigos.Models
+{
+    /// <summary>
+    /// Classe para gerenciar veículos estacionados
+    /// </summary>
+    public class Estacionamento
+    {
+        /// <summary>
+        /// Está é a lista que guardará todos os veículos estacionados.
+        /// No projeto é pedido que tenha o nome "veiculos", porém foi usado esse nome.
+        /// </summary>
+        Dictionary<string, List<string>> cadastroMotoristaVeiculo = new Dictionary<string, List<string>>();
+        
+        int? idadeMotorista { get; set; }
+        string? nomeMotorista { get; set; }
+        string?  placaVeiculo { get; set; }
+        string?  modeloVeiculo { get; set; }
+        public decimal? precoInicial { get; set; }
+        public void ListarVeiculosCadastrados(){
+            if (cadastroMotoristaVeiculo.Count() == 0) {
+                Console.WriteLine("Lista vazia");
+            }
+            else {
+                foreach (KeyValuePair<string, List<string>> item in cadastroMotoristaVeiculo) {
+                    Console.WriteLine($"{item.Key} -> {string.Join(" ", item.Value)}");
+                }
+            }
+        }
+        public void AdicionarVeiculo(string nome, int idade, string placa, string veiculo){
+            List<string> veiculosEstacionados = new List<string>();
+            try {
+                veiculosEstacionados.Add(veiculo);
+                veiculosEstacionados.Add(placa);
+                veiculosEstacionados.Add(Convert.ToString(idade));
+                cadastroMotoristaVeiculo.Add(nome, veiculosEstacionados);
+                Console.WriteLine("Cadastrado...");
+            }catch{
+                Console.WriteLine("Ocorreu um erro...");
+            }  
+        }
+        public void RemoverVeiculo(string? nome,decimal? prec, decimal horas)
+        {
+            try
+            {
+                cadastroMotoristaVeiculo.Remove(nome);
+                Console.WriteLine("Véiculo removido");
+                Console.WriteLine($"Preço final pago: R${prec*horas}");
+            }
+            catch
+            {
+                Console.WriteLine("Ocorreu um erro...");
+            }
+        }
+    }
+}
---
+++ DIO_Codigos/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using DIO_Codigos.Models;
+
+
+namespace DIO_Codigos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Esta();
+        }
+        static void Variavel()
+        {
+            Dictionary<string, List<int>> teste = new Dictionary<string, List<int>>();
+            List<int> testeArray = new List<int>();
+
+            for (int cont = 0; cont < 5; cont++)
+            {
+                testeArray.Add(cont);
+            }
+            teste.Add("Teste1", testeArray);
+            //Console.WriteLine(string.Join(", ", testeArray));
+            void Mostrar()
+            {
+                foreach (KeyValuePair<string, List<int>> item in teste)
+                {
+                    Console.WriteLine(item.Key);
+                    Console.WriteLine(string.Join(", ", item.Value));
+                    //Console.WriteLine(item);
+                }
+            }
+            Mostrar();
+        }
+        static void Esta()
+        {
+            Console.WriteLine("            INICIANDO - GESTOR DE ESTACIONAMENTOS");
+            Estacionamento novo = new Estacionamento();
+            novo.precoInicial = 10;
+            while (true)
+            {
+                Console.WriteLine("O que deseja fazer?");
+                Console.WriteLine("   1 -> listar veículos");
+                Console.WriteLine("   2 -> adicionar veículos");
+                Console.WriteLine("   3 -> remover veículos");
+                Console.WriteLine("   4 -> Sair do gerenciador");
+                Console.WriteLine("   5 -> Alterar valor do preço inicial");
+                string acionador = Console.ReadLine();
+                int entrada;
+                if (Int32.TryParse(acionador, out entrada))
+                {
+                    if (entrada == 1)
+                    {
+                        Console.WriteLine("Aqui estão todos os veículos cadastrados:");
+                        novo.ListarVeiculosCadastrados();
+                    }
+                    else if (entrada == 2)
+                    {
+                        Console.WriteLine("Digite o nome do motorista");
+                        string nomeMt = Console.ReadLine();
+                        Console.WriteLine("Digite o modelo do veículo:");
+                        string modelo = Console.ReadLine();
+                        Console.WriteLine("Digite a placa do veículo:");
+                        string placa = Console.ReadLine();
+                        Console.WriteLine("Digite a idade do motorista:");
+                        int idade = Convert.ToInt32(Console.ReadLine());
+                        novo.AdicionarVeiculo(nomeMt, idade, placa, modelo);
+                    }
+                    else if (entrada == 3)
+                    {
+                        Console.WriteLine("Digite o nome do motorista a ser removido do estacionamento:");
+                        string nomeRemoverr = Console.ReadLine();
+                        Console.WriteLine("Digite quantas horas o veículo permaneceu estacionado:");
+                        decimal horasEstacionadas = Convert.ToDecimal(Console.ReadLine());
+                        novo.RemoverVeiculo(nomeRemoverr, novo.precoInicial, horasEstacionadas);
+                    }
+                    else if (entrada == 4)
+                    {
+                        Console.WriteLine("Saindo...");
+                        break;
+                    }
+                    else if (entrada == 5)
+                    {
+                        Console.WriteLine("Digite o novo valor:");
+                        novo.precoInicial = Convert.ToDecimal(Console.ReadLine());
+                    }
+                    else
+                    {
+                        Console.WriteLine("Valor errado. Digite novamente");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Valor inválido. Digite novamente");
+                }
+                Console.WriteLine("\nPressione qualquer tecla para continuar...");
+                Console.ReadLine();
+            }
+        }
+    }
+}
